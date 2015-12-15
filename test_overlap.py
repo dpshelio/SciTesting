07edbad4d4d335ec588d587159eaba742341d64c
David PS <dps.helio@gmail.com>
2015-12-15 12:08:13 +0000

    corrected the name of what we import

--- test_overlap.py
+++ test_overlap.py
@@ -1,7 +1,7 @@
-import squares
+from squares import overlap
 
 def test_touch_on_corner():
     one = ((0, 0), (1, 1))
     two = ((1, 1), (2, 2))
-    assert overlap.overlap(one, two) == None
+    assert overlap(one, two) == None
 
